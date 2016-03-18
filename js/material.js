e7c66b3b457ab49722155002a70db745860c6df5
gmcarreon <geraldcarreon24@gmail.com>
2016-03-18 16:57:15 +0800

    Added files via upload

--- js/material.js
+++ js/material.js
@@ -1,14 +1,16 @@
 ﻿$(document).ready(function () {
  
-
     $('materialboxed').materialbox();
 
+    
+    $('.parallax').parallax();
 
     $("#submit").click(function () {
         alert("asd");
     });
-
+    
+    /*
     $(window).scroll(function() { 
     	$('.col').fadeIn(1000);
-	});
+	});*/
 });
