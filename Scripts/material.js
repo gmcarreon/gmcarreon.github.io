7e0a6e78804a4866ba7f8168b8ac1c755be85015
gmcarreon <geraldcarreon24@gmail.com>
2016-08-31 15:54:39 +0800

    Add files via upload

--- Scripts/material.js
+++ Scripts/material.js
@@ -1,5 +1,7 @@
-﻿$(document).ready(function () {
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+﻿var click = 0;
+
+$(document).ready(function () {
+
     $('.modal-trigger').leanModal();
     $('ul.tabs').tabs();
 
@@ -8,16 +10,93 @@
     });
 
     $('.collapsible').collapsible({
-        accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+        accordion: false
     });
 
-    
+
     $('.slider').slider({ full_width: false });
-    
+    $('#expert-mob').click(function(){
+        if (click == 0) {
+            click++;
+            $('.container-chart').append('<div><canvas id="canvas"></canvas></div>');
+            renderChart();
+        } else {
+            $('.container-chart div').remove();    
+            click = 0;
+        }
+    });
+    $('#expert').click(function () {
+        if (click == 0) {
+            click++;
+            $('.container-chart').append('<div><canvas id="canvas"></canvas></div>');
+            renderChart();
+        } else {
+            $('.container-chart div').remove();    
+            click = 0;
+        }
+    });
+
+    $(document).keyup(function(e){
+        if(e.keyCode == 27){
+            if($('#canvas').length != 0){
+                $('.container-chart div').remove();
+                click = 0;
+            }
+        }
+    });
     
     //$('.carousel').carousel();
     //$('.carousel.carousel-slider').carousel({ full_width: true });
 });
+
+function resume(){
+    
+    width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+    height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+    var screenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+    var screentop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+    var w = '1100';
+    var h = '600';
+    var left = ((width / 2) - (w / 2)) + screenLeft;
+    var top = ((height / 2) - (h / 2)) + screentop;
+
+    printWindow = window.open('curriculum.html', '', 'left=' + left + ',top=' + top + ',width=' + w + ',height=' + h + ',status=0');
+}
+
+function renderChart() { 
+    var dData = function () {
+        return Math.round(Math.random() * 5)
+    };
+
+    var barChartData = {
+        labels: ["C#.NET", "ASP.NET", "HTML", "CSS", "JavaScript", "Ajax", "JQuery", "JSON", "VC++", "MSSQL", "Oracle DB", "MySQL","RESTful API"],
+        datasets: [{
+            fillColor: 'rgba(255, 99, 132, .4)',
+            strokeColor: 'rgba(255, 206, 86, 1)',
+            data: [9, 7, 9, 8, 10, 9, 8, 10, 6, 9, 9, 8, 5],
+        }]
+    }
+
+    var ctx = document.getElementById("canvas").getContext("2d");
+    var barChart = new Chart(ctx).Bar(barChartData, {
+        animation: true,
+        responsive: true,
+        scaleOverride: true,
+        scaleSteps: 10,
+        scaleStartValue: 0,
+        scaleStepWidth: 1,
+        scaleFontColor: "white",
+    });
+    /*
+    var index = 11;
+    setInterval(function () {
+        barChartDemo.removeData();
+        barChartDemo.addData([dData()], "dD " + index);
+        index++;
+    }, 3000);
+    */
+}
 
 function sendEmail() {
     
